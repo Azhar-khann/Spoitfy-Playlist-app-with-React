93615bb33887289bc68b337094ef85368277a1e6
Azhar-khann <azhar.uddinn.work@gmail.com>
2023-05-17 16:57:17 +0500

    user id playlist create-> done

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -9,55 +9,50 @@ function SearchBar(props){
 
     function submit(event){
         event.preventDefault();
+        console.log(inputValue)
         props.search(inputValue);
     }  
 
     function handleChange(event){
         setInputValue(event.target.value);
-        console.log(inputValue)
     }
-/* 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(inputValue);
-      }
-    
-      const handleChange = (event) => {
-        setInputValue(event.target.value);
-    
-    } */
 
-    return (
+    if (props.user_token === '' || props.user_token === null){
+        props.url_token()
+        console.log('usertoken 1=',props.user_token)
+        return (
         <div className ="main"> 
         
             <div className="Search_bar"> 
 
-{/*             <form onSubmit={handleSubmit}>
-                <label>
-                    <input type="text" value={inputValue} onChange={handleChange} />
-               </label>
-               <button type="submit">Submit</button>
-            </form> */}
-
-{/*             <form onSubmit='https://accounts.spotify.com/authorize?response_type=token&client_id=368150b868984cf686e852f56b5d5b6f&scope=user-read-private user-read-email&redirect_uri=http://localhost:3000'>
-                <label>
-                    <input className="Search_bar_input" type="text" placeholder="Search.." onChange={handleChange}/>
-                </label>
-                <a href = 'https://accounts.spotify.com/authorize?response_type=token&client_id=368150b868984cf686e852f56b5d5b6f&scope=user-read-private user-read-email&redirect_uri=http://localhost:3000'>
-                     <button type="submit">Submit</button> 
-                </a>
-
-            </form> */}
 
                 <input className="Search_bar_input" type="text" placeholder="Search.." onChange={handleChange}/>
-                <a href = 'https://accounts.spotify.com/authorize?response_type=token&client_id=368150b868984cf686e852f56b5d5b6f&scope=user-read-private user-read-email&redirect_uri=http://localhost:3000/'>
-                     <button type="submit" onClick={submit}>Submit</button> 
+                <a href = 'https://accounts.spotify.com/authorize?response_type=token&client_id=368150b868984cf686e852f56b5d5b6f&scope=user-read-private user-read-email playlist-modify-public&redirect_uri=http://localhost:3000/'>
+                     <button type="submit" >Submit</button> 
                 </a>
-            
+              
             </div>
            
         </div>
-    )
+           
+        )
+
+    } else{
+        console.log('usertoken 2=',props.user_token)
+        return (
+            <div className ="main"> 
+            
+                <div className="Search_bar"> 
+    
+    
+                    <input className="Search_bar_input" type="text" placeholder="Search.." onChange={handleChange}/>
+                    <button type="submit" onClick={submit}>Submit</button> 
+                
+                </div>
+               
+            </div>
+            )
+    }
 
     
 }
